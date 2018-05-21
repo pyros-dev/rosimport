83f7aa0cb30d034a2d535cbfb9a45292da9f2826
AlexV <asmodehn@gmail.com>
2018-05-21 11:13:00 +0200

    adapting to latest filefinder2

--- rosimport/__init__.py
+++ rosimport/__init__.py
@@ -21,34 +21,26 @@ from ._utils import _verbose_message
 ros_path_hook = ROSDirectoryFinder.path_hook(*get_supported_ros_loaders())
 
 
-def get_rospathfinder_index_in_meta_hooks():
-    return sys.meta_path.index(ROSPathFinder)
-
-
-def get_rosdirectoryfinder_index_in_path_hooks():
-    return sys.path_hooks.index(ros_path_hook)
+py3importer = filefinder2.Py3Importer()
 
 
 def activate():
     """Install the path-based import components."""
     # We should plug filefinder first to avoid plugging ROSDirectoryFinder, when it is not a ROS thing...
 
-    filefinder2.activate()
+    py3importer.__enter__()
 
     if ros_path_hook not in sys.path_hooks:
         # We need to be before FileFinder to be able to find our '.msg' and '.srv' files without making a namespace package
         # Note this must be early in the path_hook list, since we change the logic
         # and a namespace package becomes a ros importable package.
-        sys.path_hooks.insert(filefinder2.get_filefinder_index_in_path_hooks(), ros_path_hook)
+        ff2_idx = sys.path_hooks.index(filefinder2.ff_path_hook)
+        sys.path_hooks.insert(ff2_idx, ros_path_hook)
 
     if ROSPathFinder not in sys.meta_path:
         # adding metahook, before the usual pathfinder, to avoid interferences with python namespace mechanism...
-        sys.meta_path.insert(filefinder2.get_pathfinder_index_in_meta_hooks(), ROSPathFinder)
-
-    # Resetting sys.path_importer_cache
-    # to support the case where we have an implicit (msg/srv) package inside an already loaded package,
-    # since we need to replace the default importer.
-    sys.path_importer_cache.clear()
+        pf2_idx = sys.meta_path.index(filefinder2.PathFinder)
+        sys.meta_path.insert(pf2_idx, ROSPathFinder)
 
 
 def deactivate():
@@ -56,23 +48,17 @@ def deactivate():
     # initialized finders will remain in sys.path_importer_cache
 
     # removing metahook
-    sys.meta_path.pop(get_rospathfinder_index_in_meta_hooks())
+    ff2_idx = sys.path_hooks.index(ros_path_hook)
+    sys.meta_path.pop(ff2_idx)
     # removing path_hook
-    sys.path_hooks.pop(get_rosdirectoryfinder_index_in_path_hooks())
+    pf2_idx = sys.meta_path.index(ROSPathFinder)
+    sys.path_hooks.pop(pf2_idx)
 
-    filefinder2.deactivate()
-
-    # Resetting sys.path_importer_cache to get rid of previous importers
-    sys.path_importer_cache.clear()
+    py3importer.__exit__(None, None, None)
 
 
 __all__ = [
     'MsgDependencyNotFound',
-    'generate_msgsrv_nspkg',
     'ROSMsgLoader',
     'ROSSrvLoader',
-    'genmsg_py',
-    'gensrv_py',
-    'activate_hook_for',
-    'deactivate_hook_for',
 ]
